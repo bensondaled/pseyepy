05b6f8931d81a66d8dd0149d054eb3ae5c3015d3
Ben Deverett <bendeverett@gmail.com>
2017-11-28 12:54:57 -0500

    trying to explicitly add lib path for lusb

--- setup.py
+++ setup.py
@@ -10,6 +10,9 @@ if sys.platform in ('darwin','linux','linux2'):
     libusb_incl = subprocess.check_output(['pkg-config', '--cflags', 'libusb-1.0']).strip().decode('utf8')
     libusb_incl = libusb_incl.replace('-I','').split(' ')
 
+    libusb_libpath = subprocess.check_output(['pkg-config', '--libs', 'libusb-1.0']).strip().decode('utf8')
+    libusb_libpath = libusb_libpath.replace('-L','').split(' ')[0]
+
 elif sys.platform.startswith('win'):
     warnings.warn('Setup params not yet configured for Windows. Setup will only work if libusb library paths are automatically found.')
 
@@ -23,6 +26,7 @@ extensions = [  Extension('pseyepy.cameras',  # I think this will be pseyepy.pse
                 extra_compile_args=['-std=c++11','-stdlib=libc++'],
                 extra_link_args=['-std=c++11','-stdlib=libc++'],
                 include_dirs=['pseyepy/src']+libusb_incl,
+                library_dirs=[libusb_libpath],
                 libraries=['usb'],
             )]
 
