df2020973c4afb4b5f5c39ee1a6ba504dc055696
Ben Deverett <bendeverett@gmail.com>
2017-12-02 14:20:19 -0500

    more general installation

--- setup.py
+++ setup.py
@@ -1,33 +1,65 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
+from sysconfig import get_paths
 import os, sys
 import warnings
 import subprocess
 
-
+### install libusb
+# keeping this slightly hacky approach to guarantee that the correct libusb is used and is easily findable
 if sys.platform in ('darwin','linux','linux2'):
-    libusb_incl = subprocess.check_output(['pkg-config', '--cflags', 'libusb-1.0']).strip().decode('utf8')
-    libusb_incl = libusb_incl.replace('-I','').split(' ')
+    python_data_path = get_paths()['data']
 
-    libusb_libpath = subprocess.check_output(['pkg-config', '--libs', 'libusb-1.0']).strip().decode('utf8')
-    libusb_libpath = libusb_libpath.replace('-L','').split(' ')[0] # 0 for path, b/c second item is often the library itself; this is a bit hacky
+    wd = os.path.join('.','pseyepy','ext')
+    subprocess.call('tar -jxf libusb-1.0.21.tar.bz2', cwd=wd, shell=True)
+
+    wd = os.path.join('.','pseyepy','ext','libusb-1.0.21')
+    subprocess.call('sudo ./configure --prefix={}'.format(python_data_path), cwd=wd, shell=True)
+    subprocess.call('make clean', cwd=wd, shell=True)
+    subprocess.call('sudo make', cwd=wd, shell=True)
+    subprocess.call('sudo make install', cwd=wd, shell=True)
+
+    wd = os.path.join('.','pseyepy','ext')
+    subprocess.call('mkdir -p include && mkdir -p lib', cwd=wd, shell=True)
+
+    src_include = os.path.join(python_data_path, 'include', 'libusb-1.0')
+    dest_include = os.path.join('.', 'pseyepy', 'ext', 'include', 'libusb-1.0')
+    subprocess.call(['ln -s {} {}'.format(src_include, dest_include)], shell=True, cwd='.')
+
+    for ln in ['libusb-1.0.0.dylib', 'libusb-1.0.a', 'libusb-1.0.dylib', 'libusb-1.0.la']:
+        src_lib = os.path.join(python_data_path, 'lib', ln)
+        dest_lib = os.path.join('.', 'pseyepy', 'ext', 'lib', ln)
+        subprocess.call(['ln -s {} {}'.format(src_lib, dest_lib)], shell=True, cwd='.')
+
+    libusb_incl = ['pseyepy/ext/include/libusb-1.0']
+    libusb_libpath = 'pseyepy/ext/lib'
 
 elif sys.platform.startswith('win'):
     warnings.warn('Setup params not yet configured for Windows. Setup will only work if libusb library paths are automatically found.')
+    libusb_incl = []
+    libusb_libpath = ''
 
+### setup params
 os.environ["CC"]= "g++"
-
 srcs = ['pseyepy/src/ps3eye.cpp','pseyepy/src/ps3eye_capi.cpp','pseyepy/cameras.pyx']
-
-extensions = [  Extension('pseyepy.cameras',  # I think this will be pseyepy.pseyepy.cameras when I don't build inplace
+extensions = [  Extension('pseyepy.cameras',
                 srcs, 
                 language='c++',
                 extra_compile_args=['-std=c++11','-stdlib=libc++'],
                 extra_link_args=['-std=c++11','-stdlib=libc++'],
                 include_dirs=['pseyepy/src']+libusb_incl,
                 library_dirs=[libusb_libpath],
-                libraries=['usb'],
+                libraries=['usb-1.0'],
             )]
 
-setup(ext_modules=cythonize(extensions),)
+### run setup
+setup(  name='pseyepy',
+        version='0.0',
+        description='pseyepy camera package',
+        author='Ben Deverett',
+        author_email='deverett@princeton.edu',
+        url='https://github.com/bensondaled/pseyepy',
+        packages=['pseyepy'],
+        package_data={'pseyepy': ['cameras.pyx']},
+        ext_modules=cythonize(extensions),)
